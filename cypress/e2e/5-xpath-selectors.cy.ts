111426cfa4069a7cafe17aecbe1c3457f15367bf
anvi <avikhlyantsev@fix.ru>
2024-06-23 14:03:05 +0300

    section 5 finished

--- cypress/e2e/5-xpath-selectors.cy.ts
+++ cypress/e2e/5-xpath-selectors.cy.ts
@@ -1,12 +1,18 @@
 describe('Locators', () => {
     beforeEach(() => {
-        cy.visit('/dynamicid');
+        cy.visit('/classattr');
     })
 
     it("how to find an elemetnt by its text", () => {
-        cy.xpath(`//*[text='Correct variant is']`)
-            .should('contain.text', 'Correct');
+        cy.xpath(`//*[text()='Correct variant is']`).should(
+            'contain.text',
+            'Correct');
     })
 
+    it('find an element by its attr using xpath', () => {
+        cy.xpath(`//pre[@class=' language-html']`).should(
+            'contain.text',
+            'button');
+    })
 
 })
